ccf87cdee2bf3cfdf85f1b72a4928dd46ae428ce
Kevin <kevincorsigon@hotmail.com>
2021-03-20 00:52:01 -0300

    initial

---
+++ build/index.js
@@ -0,0 +1,115 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const md5_1 = __importDefault(require("md5"));
+const axios_1 = __importDefault(require("axios"));
+const redis_1 = require("redis");
+const app = express_1.default();
+const PORT = 4000;
+const MARVEL_PUBLIC_KEY = '76d403d5fe8da085131b25ca6462a968';
+const MARVEL_PRIVATE_KEY = '886c2f1cdb35a4436c7d5ca59ed8ef31db0efd2d';
+const redisClient = new redis_1.RedisClient({
+    port: 6379,
+    host: 'localhost'
+});
+app.listen(PORT, () => {
+    console.log(`Aplicação escutando na porta ${PORT}`);
+});
+app.get("/hero", (req, res) => {
+    let ts = ticks();
+    let hash = hashMd5(ts, MARVEL_PUBLIC_KEY, MARVEL_PRIVATE_KEY);
+    let uri = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${MARVEL_PUBLIC_KEY}&hash=${hash}&limit=100`;
+    redisGet(`hero`, function (data) {
+        if (data == null) {
+            axios_1.default.get(uri)
+                .then(response => {
+                let array = [];
+                for (let item of response.data.data.results) {
+                    var hero = {
+                        name: item.name,
+                        descripition: item.description,
+                        imageUrl: item.thumbnail.path + "." + item.thumbnail.extension,
+                        wikiUrl: item.urls[1].url
+                    };
+                    array.push(hero);
+                    redisSet(`hero/${item.name}`, hero);
+                }
+                redisSet(`hero`, array);
+                res.json(array);
+            })
+                .catch(error => {
+                console.log(error);
+                res.json(error);
+            });
+        }
+        else {
+            res.json(data);
+        }
+    });
+});
+app.get("/hero/:name", (req, res) => {
+    const { name } = req.params;
+    let ts = ticks();
+    let hash = hashMd5(ts, MARVEL_PUBLIC_KEY, MARVEL_PRIVATE_KEY);
+    let uri = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${MARVEL_PUBLIC_KEY}&hash=${hash}&name=${name}`;
+    redisGet(`hero/${name}`, function (data) {
+        if (data == null) {
+            axios_1.default.get(uri)
+                .then(response => {
+                var result = response.data.data.results[0];
+                var hero = {
+                    name: result.name,
+                    descripition: result.description,
+                    imageUrl: result.thumbnail.path + "." + result.thumbnail.extension,
+                    wikiUrl: result.urls[1].url
+                };
+                redisSet(`hero/${result.name}`, hero);
+                res.json(hero);
+            })
+                .catch(error => {
+                console.log(error);
+                res.json(error);
+            });
+        }
+        else {
+            data.fromCache = true;
+            res.json(data);
+        }
+    });
+});
+let ticks = function () {
+    var d = new Date();
+    var dStart = new Date(1970, 1, 1);
+    return ((d.getTime() - dStart.getTime()) * 10000);
+};
+let hashMd5 = function (ts, publicKey, privateKey) {
+    return md5_1.default(ts + privateKey + publicKey);
+};
+let redisSet = function (key, data) {
+    let obj = null;
+    redisClient.get(key, (err, value) => {
+        if (value == undefined) {
+            redisClient.set(key, JSON.stringify(data), (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
+        obj = JSON.parse(value);
+    });
+    return obj;
+};
+let redisGet = function (key, callback) {
+    redisClient.get(key, (err, value) => {
+        if (value != undefined) {
+            callback(JSON.parse(value));
+        }
+        else {
+            callback(null);
+        }
+    });
+};
+//# sourceMappingURL=index.js.map
